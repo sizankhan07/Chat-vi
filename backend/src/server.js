3a7959260595cf18858660dd825077d5a8f453c8
sizan khan <sizankhan182@gmail.com>
2025-11-18 19:35:06 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
+
+const app= express();
+
+const PORT =process.env.PORT || 3000;
+
+app.use("/api/auth",authRoutes);
+
+app.listen(3000,()=>console.log("server running on PORT:"+PORT));
