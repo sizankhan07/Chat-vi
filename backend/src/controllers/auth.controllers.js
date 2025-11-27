5c1bf99a809c6279e6f59ddcecb62a452337d36a
sizan khan <sizankhan182@gmail.com>
2025-11-27 13:14:22 +0530

    update profile endpoint added

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -3,6 +3,8 @@ import { generateToken } from "../lib/utils.js";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import {ENV} from "../lib/env.js"
+import cloudinary from "../lib/cloudinary.js";
+
 
 
 
@@ -95,4 +97,24 @@ export const logout = (_, res) => {
   res.cookie("jwt", "", { maxAge: 0 });
   res.status(200).json({ message: "Logged out successfully" });
 };
+export const updateProfile = async (req, res) => { try {
+    const { profilePic } = req.body;
+    if (!profilePic) return res.status(400).json({ message: "Profile pic is required" });
 
+    const userId = req.user._id;
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { profilePic: uploadResponse.secure_url },
+      { new: true }
+    );
+
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log("Error in update profile:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+  
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 
 import express from "express";
-
+import cookieParser from "cookie-parser"
 import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
@@ -13,9 +13,10 @@ const app= express();
 
 const  __dirname =path.resolve();
 
-const PORT =ENV.PORT || 3000;
+const PORT = ENV.PORT || 3000;
 
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
 
