b5fb0d5a8a87f8d9ec7cc9225a670737b008592f
sizan khan <sizankhan182@gmail.com>
2025-11-28 15:35:35 +0530

    signup frontend and controlllers

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@
 import express from "express";
 import cookieParser from "cookie-parser"
 import path from "path";
+import cors from "cors"
 
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
@@ -17,6 +18,7 @@ const  __dirname =path.resolve();
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json());
+app.use(cors({origin:ENV.CLIENT_URL,Credential:true}));
 app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
