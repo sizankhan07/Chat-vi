e11194816c6cd82182593a604ececdcf528ac2a6
sizan khan <sizankhan182@gmail.com>
2025-12-01 15:37:44 +0530

    main vhange b

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,6 @@
-import axios from "axios"
+import axios from "axios";
+
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://chat-fy.onrender.com" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
   withCredentials: true,
 });
