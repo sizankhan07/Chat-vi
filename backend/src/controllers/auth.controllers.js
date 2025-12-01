eb1383061abb40d0f9c5574bc8e36618837e37e8
sizan khan <sizankhan182@gmail.com>
2025-12-01 13:47:30 +0530

    up1

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -1,4 +1,4 @@
-import { sendWelcomeEmail } from "../emails/emailHandlers.js";
+
 import { generateToken } from "../lib/utils.js";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
